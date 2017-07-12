d3dae943a4cde6e32203bdc37e7d137aa4179170
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 17:11:17 +0000

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end 
+  
+  def copyright_generator
+    GuViewTool::Renderer.copyright 'Edward Gu', 'All rights reserved'
+  end
 end
