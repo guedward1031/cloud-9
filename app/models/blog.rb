d6da2c546ed7fc3aca06104404744b483740b3ba
Jordan Hudgens <jordan@devcamp.com>
2017-08-07 15:09:19 +0000

    Added validations for topics in blogs and added a topic form element

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,7 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :topic_id
 
   belongs_to :topic
 
@@ -12,10 +12,10 @@ class Blog < ApplicationRecord
   def self.special_blogs
     all
   end
-    def self.featured_blogs
+  def self.featured_blogs
     limit(2)
   end
-    def self.recent
+  def self.recent
     order("created_at DESC")
   end
 end
